cfdbf4759e911f6780dc222e8b27a2a63a72c7fa
[firstname lastname] <[valid-email]>
2026-02-09 14:50:07 +0100

    14:49 9.2

---
+++ dashboard.php
@@ -0,0 +1,30 @@
+<?php
+
+    echo "dostal jsem:".$_POST['uname']."<br>";
+    echo "dostal jsem:".$_POST['psw']."<br>";
+    session_start();
+    if($_POST["uname"] == "admin" && $_POST["psw"] == "admin"){
+        $_SESSION["uname"] = $_POST["uname"];
+        $_SESSION["psw"] = $_POST["psw"];
+    }
+
+    if (isset($_SESSION["uname"])){
+        $pass = $_SESSION["psw"];
+        $user = $_SESSION["uname"];
+
+        echo "uzivatel $user a heslo $pass";
+
+        echo ' <form action="" method="get">
+            <input type="submit" name="logout" value="odhlasit se">
+        </form>';
+
+        echo isset($_GET["logout"]) ? "odhlasit":"neodhlasit";
+        if(isset($_GET["logout"])){
+            session_unset();
+            session_destroy();
+        }
+    }
+    else{
+        echo "uzivatel neni prihlasen";
+    }
+?>
