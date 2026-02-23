57664b25b8a5756d8ca8b759787f9415c8042f8d
[firstname lastname] <[valid-email]>
2026-02-23 15:19:59 +0100

    1

---
+++ databaze.php
@@ -0,0 +1,27 @@
+<?php
+
+$dsn = "mysql:host=localhost;dbname=test;charset=utf8";
+$username = "testuser";
+$password = "testuser";
+
+try {
+    $db = new PDO($dsn, $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo"jsem zde";
+}
+
+catch (PDOException $e) {
+    echo "Nelze se pripojit k databázi: " . $e->getMessage();
+    //exit();
+}
+
+function getAll($table) {
+    global $db;
+    $sql = "SELECT * FROM $table";
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+?>
